6538a3b997f0ee631f81c440340fe7121ba176a6
Tim Douglas <neurotech@gmail.com>
2017-03-15 17:52:19 +1100

    Simple install script

--- install.sh
+++ install.sh
@@ -1,1 +1,12 @@
-#!/bin/bash
+#!/bin/sh
+
+# super
+sudo -v
+
+# Link repo files to ~
+echo "Linking config files to $HOME"
+ln -sf $PWD/.tmux.conf $HOME/.tmux.conf
+
+# Reload zsh
+echo "Refreshing zsh"
+source ~/.zshrc

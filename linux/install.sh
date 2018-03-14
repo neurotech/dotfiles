5a0f857e1b744b76a8667a404f9872cc311ad65a
Tim Douglas <neurotech@gmail.com>
2018-03-14 18:32:17 +1100

    Removed rofi, updated install

--- linux/install.sh
+++ linux/install.sh
@@ -1,17 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
 # Directories
+echo "Creating directories."
 mkdir -p $HOME/.config/i3
-mkdir -p $HOME/.config/rofi
 
 # Links
-echo "Soft linking configuration files to $HOME"
+echo "Soft linking configuration files."
 ln -sf $PWD/.Xresources $HOME/.Xresources
 ln -sf $PWD/.zshrc $HOME/.zshrc
 ln -sf $PWD/i3/config $HOME/.config/i3/config
-ln -sf $PWD/rofi/config $HOME/.config/rofi/config
-ln -sf $PWD/rofi/reddy.rasi $HOME/.config/rofi/reddy.rasi
 
 # Reload zsh
-echo "Refreshing zsh"
+echo "Reloading zsh."
 source $HOME/.zshrc

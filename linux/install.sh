382ca0ec3f65a9bb063e89aabe2e8e5c34c665e5
Tim Douglas <neurotech@gmail.com>
2018-03-16 09:22:48 +1100

    sudo ln

--- linux/install.sh
+++ linux/install.sh
@@ -10,7 +10,7 @@ ln -sf $PWD/.Xresources $HOME/.Xresources
 ln -sf $PWD/.zshrc $HOME/.zshrc
 ln -sf $PWD/i3/config $HOME/.config/i3/config
 ln -sf $PWD/rofi/config $HOME/.config/rofi/config
-ln -sf $PWD/rofi/hovercastle.theme /usr/share/rofi/themes/hovercastle.theme
+sudo ln -sf $PWD/rofi/hovercastle.theme /usr/share/rofi/themes/hovercastle.theme
 
 # Reload zsh
 echo "Reloading zsh."

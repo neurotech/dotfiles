d3e9dc8b37f1355ef5a261f3baaf5486f3384c1c
Tim Douglas <neurotech@gmail.com>
2018-03-16 09:20:54 +1100

    Added rofi links to install

--- linux/install.sh
+++ linux/install.sh
@@ -9,6 +9,8 @@ echo "Soft linking configuration files."
 ln -sf $PWD/.Xresources $HOME/.Xresources
 ln -sf $PWD/.zshrc $HOME/.zshrc
 ln -sf $PWD/i3/config $HOME/.config/i3/config
+ln -sf $PWD/rofi/config $HOME/.config/rofi/config
+ln -sf $PWD/rofi/hovercastle.theme /usr/share/rofi/themes/hovercastle.theme
 
 # Reload zsh
 echo "Reloading zsh."

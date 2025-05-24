7a3290b95b5192c1c5593b89f7057bc1c70a76fc
Tim Douglas <neurotech@gmail.com>
2025-05-24 20:35:18 +1000

    Further beyond

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,5 +1,3 @@
 function fish_prompt
-    printf '[%s] %s%s@%s %s%s %s%s%s \n> ' (date "+%H:%M:%S") (set_color 4F39F6) \
-        $USER (prompt_hostname) (set_color FFD230) $PWD \
-        (set_color normal)
+    printf (set_color 4F39F6) $USER (prompt_hostname) (set_color FFD230) $PWD (set_color normal)
 end

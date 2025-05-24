c47580bd1b29839aba7e91b9fc62e67e4f9d7edc
Tim Douglas <neurotech@gmail.com>
2025-05-24 20:31:02 +1000

    even simpler

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,7 +1,5 @@
 function fish_prompt
-    set -l pipestatus_string (__fish_print_pipestatus "[" "]" "|")
-
     printf '[%s] %s%s@%s %s%s %s%s%s \n> ' (date "+%H:%M:%S") (set_color 4F39F6) \
-        $USER (prompt_hostname) (set_color FFD230) $PWD $pipestatus_string \
+        $USER (prompt_hostname) (set_color FFD230) $PWD \
         (set_color normal)
 end

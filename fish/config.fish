fb2702bf6786caebe88d88caa670a6073d68efb9
Tim Douglas <neurotech@gmail.com>
2025-05-24 18:49:29 +1000

    More fish customisation

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,13 @@
-if [ -f ~/.bash_aliases ]; then
-  . ~/.bash_aliases
-fi
+# Disable the fish greeting message
+set fish_greeting ""
 
-set brewcmd (path filter /opt/homebrew/bin/brew /usr/local/bin/brew)[1]
-and $brewcmd shellenv | source
+# Setup brew
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+# if [ -f ~/.bash_aliases ]; then
+#   . ~/.bash_aliases
+# fi
+
+
+# set brewcmd (path filter /opt/homebrew/bin/brew /usr/local/bin/brew)[1]
+# and $brewcmd shellenv | source

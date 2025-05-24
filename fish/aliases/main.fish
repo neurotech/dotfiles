b64f76eae96d2e15c7c7e4df05b95f1bd15bc235
Tim Douglas <neurotech@gmail.com>
2025-05-24 19:46:41 +1000

    Simple prompt for now

--- fish/aliases/main.fish
+++ fish/aliases/main.fish
@@ -1,3 +1,4 @@
 alias ls="eza --long --classify --all --header --git --no-user --tree --level 1"
 alias cat="bat"
 alias brightness="brightnessctl -d intel_backlight set"
+alias dotfiles="cd $HOME/dotfiles/ && git pull && ./install.sh && cd -"

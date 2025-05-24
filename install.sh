875e220473437e6adfbe0c973024e0e429c0a24a
Tim Douglas <neurotech@gmail.com>
2025-05-24 20:17:42 +1000

    Update install script again

--- install.sh
+++ install.sh
@@ -1,37 +1,37 @@
-#!/bin/sh
+#!/home/linuxbrew/.linuxbrew/bin/fish
 
 # Link fish config
-if [ ! -f "$HOME/.config/fish/config.fish" ]; then
-  echo "Linking fish config to $HOME/.config/fish/config.fish..."
-  mkdir -p $HOME/.config/fish
-  ln -sf $PWD/fish/config.fish $HOME/.config/fish/config.fish
+if not test -f "$HOME/.config/fish/config.fish"
+    echo "Linking fish config to $HOME/.config/fish/config.fish..."
+    mkdir -p "$HOME/.config/fish"
+    ln -sf "$PWD/fish/config.fish" "$HOME/.config/fish/config.fish"
 else
-  echo "Fish config already linked."
-fi
+    echo "Fish config already linked."
+end
 
 # Link fish aliases
-if [ ! -f "$HOME/.config/fish/aliases/main.fish" ]; then
-  echo "Linking fish aliases to $HOME/.config/fish/aliases/main.fish..."
-  mkdir -p $HOME/.config/fish/aliases
-  ln -sf $PWD/fish/aliases/main.fish $HOME/.config/fish/aliases/main.fish
+if not test -f "$HOME/.config/fish/aliases/main.fish"
+    echo "Linking fish aliases to $HOME/.config/fish/aliases/main.fish..."
+    mkdir -p "$HOME/.config/fish/aliases"
+    ln -sf "$PWD/fish/aliases/main.fish" "$HOME/.config/fish/aliases/main.fish"
 else
-  echo "Fish aliases already linked."
-fi
+    echo "Fish aliases already linked."
+end
 
 # Link fish functions (main)
-if [ ! -f "$HOME/.config/fish/functions/main.fish" ]; then
-  echo "Linking fish functions (main) to $HOME/.config/fish/functions/main.fish..."
-  mkdir -p $HOME/.config/fish/functions
-  ln -sf $PWD/fish/functions/main.fish $HOME/.config/fish/functions/main.fish
+if not test -f "$HOME/.config/fish/functions/main.fish"
+    echo "Linking fish functions (main) to $HOME/.config/fish/functions/main.fish..."
+    mkdir -p "$HOME/.config/fish/functions"
+    ln -sf "$PWD/fish/functions/main.fish" "$HOME/.config/fish/functions/main.fish"
 else
-  echo "Fish functions (main) already linked."
-fi
+    echo "Fish functions (main) already linked."
+end
 
 # Link fish functions (fish_prompt)
-if [ ! -f "$HOME/.config/fish/functions/fish_prompt.fish" ]; then
-  echo "Linking fish functions (fish_prompt to $HOME/.config/fish/functions/fish_prompt.fish..."
-  mkdir -p $HOME/.config/fish/functions
-  ln -sf $PWD/fish/functions/fish_prompt.fish $HOME/.config/fish/functions/fish_prompt.fish
+if not test -f "$HOME/.config/fish/functions/fish_prompt.fish"
+    echo "Linking fish functions (fish_prompt) to $HOME/.config/fish/functions/fish_prompt.fish..."
+    mkdir -p "$HOME/.config/fish/functions"
+    ln -sf "$PWD/fish/functions/fish_prompt.fish" "$HOME/.config/fish/functions/fish_prompt.fish"
 else
-  echo "Fish functions (fish_prompt already linked."
-fi
+    echo "Fish functions (fish_prompt) already linked."
+end

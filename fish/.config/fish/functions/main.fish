8c3c26504d92eafbe7f8e4e93953f440aba6881c
Tim Douglas <neurotech@gmail.com>
2026-01-22 13:29:18 +1100

    Adjust PWD colour

--- fish/.config/fish/functions/main.fish
+++ fish/.config/fish/functions/main.fish
@@ -10,7 +10,7 @@ end
 function fish_prompt
     printf '%s%s%s%s%s%s %s %s%s%s%s%s%s %s \n> ' \
         (set_color 7C86FF) $USER (set_color C6D2FF) "@" (set_color A3B3FF) (prompt_hostname) \
-        "💽" \
-        (set_color 44403B) "[" (set_color FFD230) (string replace $HOME '~' $PWD) (set_color 44403B) "]" \
+        "🏯" \
+        (set_color 44403B) "[" (set_color 00D492) (string replace $HOME '~' $PWD) (set_color 44403B) "]" \
         (set_color normal)
 end

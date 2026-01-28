79c0eb96a120e44b417079a4df1d8ca5bfde8384
Tim Douglas <neurotech@gmail.com>
2026-01-28 19:40:36 +1100

    Update config.fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,6 +5,12 @@ end
 # Disable the fish greeting message
 set -U fish_greeting
 
+# Fix for Tauri apps
+set -gx WEBKIT_DISABLE_DMABUF_RENDERER 1
+
+# claude
+set -gx PATH $PATH $HOME/.local/bin
+
 # Functions
 if [ -f $HOME/.config/fish/functions/main.fish ]
     source $HOME/.config/fish/functions/main.fish
